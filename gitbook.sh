5b7f49a542278b70e39c74e88f4f38ed8cccebb2
Zhao Tianze <zen_zhao@qq.com>
2018-10-18 11:35:41 +0800

    增加gitbook脚本发布脚本

---
+++ gitbook.sh
@@ -0,0 +1,14 @@
+#bash
+
+git checkout master
+rm -rf _book
+gitbook build
+git add .
+git commit -m $1
+git push -u origin master
+git checkout gh-pages
+cp -r _book/* .
+git add .
+git commit -m $1
+git push -u origin gh-pages
+git checkout master
